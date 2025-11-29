5e51be39b8f4dd99f3bfe6b3e3285c3940cb3f07
amaryasser7 <yamar8861@gmail.com>
2025-11-29 10:35:46 +0200

    Add PlatesCounter and burning/frying systems
    Introduced PlatesCounter prefab and script, new kitchen objects (MeatPattyCooked, MeatPattyBurned), and associated ScriptableObjects for burning and frying recipes. Updated CuttingCounter and StoveCounter prefabs and scripts to support progress bars and new cooking logic. Added ProgressBarUI prefab and interface for progress tracking, and included a new audio mixer asset.

---
+++ Assets/scripts/Counters/PlatesCounter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesCounter : MonoBehaviour
+{
+    [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+
+    private float spwanPlateTimer;
+    private float spwanPlateTimerMax = 4f;
+
+    private void Update()
+    {
+        spwanPlateTimer +=Time.deltaTime;
+        if  
+    }
+}
